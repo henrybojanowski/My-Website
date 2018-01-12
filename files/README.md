c11e881d25c2dd0747c830a516afcb62f2c743fa
Henry Bojanowski <henryboj@bu.edu>
2018-01-12 17:34:33 -0500

    added new file and made some changes

---
+++ files/README.md
@@ -0,0 +1,2 @@
+# My-Website
+My website with academic information about myself created with HTML, CSS, Javascript and jQuery
---
+++ files/myWebsite.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+
+	$('.boring_or_fun').hide();
+	$('.boring_or_funny').hide();
+	$('.on_or_off').hide();
+
+	//CS 111 stuff begins 
+	$('.CS-111-button').mouseenter(function(){
+		$(this).addClass('bold');
+	});
+	$('.CS-111-button').mouseleave(function(){
+		$(this).removeClass('bold');
+	});
+
+	$('.CS-111-button').click(function(){
+		$('.boring_or_fun').show();
+	});
+
+	//CS 111 stuff ends 
+
+	//CS-112 button stuff begins
+
+	$('.CS-112-button').mouseenter(function(){
+		$(this).addClass('bold');
+	});
+	$('.CS-112-button').mouseleave(function(){
+		$(this).removeClass('bold');
+	});
+
+	$('.CS-112-button').click(function(){
+		$('.boring_or_funny').show();
+	});
+	
+
+	//CS-112 button stuff ends
+
+	$('h1').hover(function(){
+		$(this).addClass('blue');
+	});
+
+
+	$('.expand').mouseenter(function(){
+		$(this).addClass('bold');
+	});
+	
+	$('.expand').mouseleave(function(){
+		$(this).removeClass('bold');
+	});
+
+	$('.expand').click(function(){
+		$('.on_or_off').show();
+	});
+
+	
+
+	
+	
+
+});
