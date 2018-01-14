7f31e0f0f79a8eb2ff9dc098a8d5781daa46b38c
Henry Bojanowski <henryboj@bu.edu>
2018-01-14 15:55:26 -0500

    Added php features

--- files/functions.php
+++ files/functions.php
@@ -2,7 +2,14 @@
 	
 	function sayHello()
 	{
-		echo "Hello World" . "<br>";
+		echo "Hello! Welcome to my website!" . "<br>";
+		date_default_timezone_set("America/New_York");
+		// print date("m/d/y G.i:s<br>", time());
+   		print "Today is ";
+   		print date("m/d/Y");
+   		echo "<br>";
+   		print date("g:i a");
+
 	}
 
 	
